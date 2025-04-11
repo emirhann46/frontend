68e08a593fa064ac8fb9aca94b411530bdd8e739
EthemTutma <ethem.tutma@gmail.com>
2025-04-11 14:46:12 +0300

    dcscdscds

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -24,7 +24,7 @@ export default function CartPage() {
     {
       id: "1",
       eventId: "1",
-      eventTitle: "Duman Konserii",
+      eventTitle: "Duman Konseri",
       eventImage: "https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
       eventDate: "15 Aralık 2023",
       eventTime: "20:00",
